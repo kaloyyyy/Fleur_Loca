e94f6db7ec93079412364eef2ee75b9bd981a85e
Kervy Yu <kerbs.yu@gmail.com>
2024-03-15 23:06:27 +0800

    Delete Fleur_Loca directory

--- Fleur_Loca/admin/js/admin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var el = document.getElementById("wrapper");
-    var toggleBtn = document.getElementById("menu-toggle");
-
-    toggleBtn.onclick = function() {
-        el.classList.toggle("toggled");
-    };
-});
