a35f1ce537755175eeaafb4042e51d695c66876d
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2024-11-27 09:02:15 +0800

    Revert "Delete Fleur_Loca directory"
    This reverts commit e94f6db7ec93079412364eef2ee75b9bd981a85e.

---
+++ Fleur_Loca/javascript/footer.js
@@ -0,0 +1,44 @@
+document.write('\
+<link rel="stylesheet" href="css/footer.css" />\
+<footer>\
+      <div class="container p-4">\
+        <div class="row">\
+          <div class="description col-lg-4 col-md-12 mb-4">\
+            <a class="mb-3">\
+              <a class="navbar-brand" href="index.php"><img src="/Fleur_Loca/images/fleurlogo.png" alt=""/></a>\
+            <p>\
+            From the choice of blooms to the overall design, we pride ourselves on our ability to create stunning wedding florals that align with your style and budget bringing your wedding day vision to life.\
+            </p>\
+          </div>\
+          <div class="links col-lg-4 col-md-6 mb-4">\
+            <h5 class="mb-3">links</h5>\
+            <ul class="list-unstyled  mb-0">\
+              <li class="mb-2">\
+                <a href="/Fleur_Loca/index.php">Home</a>\
+              </li>\
+              <li class="mb-2">\
+                <a href="/Fleur_Loca/gallerypage.php">Gallery</a>\
+              </li>\
+              <li class="mb-2">\
+                <a href="/Fleur_Loca/aboutpage.php">About</a>\
+              </li>\
+              <li>\
+                <a href="/Fleur_Loca/contactpage.php" >Contact</a>\
+              </li>\
+            </ul>\
+          </div>\
+          <div class="short col-lg-4 col-md-6 mb-4">\
+            <h5 class="mb-3">Contact Us</h5>\
+              <p class="contact-icon"><i class="fas fa-home mr-3"></i>Bayswater WA 6053 & workshop’s @ Dayton WA 6055</p>\
+              <p class="contact-icon"><i class="fas fa-envelope mr-3"></i> fleurloca@outlook.com</p>\
+              <div class="social-icon mt-4">\
+                <!-- Facebook -->\
+                 <a type="button" href="https://www.facebook.com/FleurLocaFlorist" target="_blank" class="btn btn-floating btn-lg"><i class="fab fa-facebook-f"></i></a>\
+                <!-- Instagram -->\
+                <a type="button" href="https://www.instagram.com/fleurlocaflorist" target="_blank" class="btn btn-floating btn-lg"><i class="fab fa-instagram"></i></a>\
+              </div>\
+          </div>\
+        </div>\
+      </div>\
+    </footer>\
+');
